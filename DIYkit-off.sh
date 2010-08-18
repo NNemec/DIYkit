eacda466406619d988ae6af97dd5c3894c3b9a17
Norbert Nemec <Norbert@Nemec-online.de>
2010-08-18 14:29:35 +0100

    Minor cleanup

--- DIYkit-off.sh
+++ DIYkit-off.sh
@@ -1,4 +1,4 @@
-[ -z "$DIYkit" ] && export DIYkit=~/NNlab
+[ -z "$DIYkit" ] && export DIYkit=~/.DIYkit
 
 PATH_REMOVE() {
     echo :$1 | sed "s#:$2##g" | cut -c 2-
@@ -13,4 +13,4 @@ export LD_LIBRARY_PATH=`PATH_REMOVE "$LD_LIBRARY_PATH" $DIYkit/lib`
 export PYTHONPATH=`PATH_REMOVE "$PYTHONPATH" $DIYkit/lib/python2.6/site-packages`
 export PKG_CONFIG_PATH=`PATH_REMOVE "$PKG_CONFIG_PATH" $DIYkit/lib/pkgconfig`
 
-unset PATH_REMOVE NNlab DIYkit
+unset PATH_REMOVE DIYkit
